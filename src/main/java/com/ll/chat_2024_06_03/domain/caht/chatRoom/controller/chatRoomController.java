a9f1a0602ec7626f1af107950e3b945edb71ae7d
2251341 <dydwksos246@naver.com>
2024-06-03 15:09:53 +0900

    @RequstMapping, showMake Action

--- src/main/java/com/ll/chat_2024_06_03/domain/caht/chatRoom/controller/chatRoomController.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/caht/chatRoom/controller/chatRoomController.java
@@ -1,20 +1,33 @@
 package com.ll.chat_2024_06_03.domain.caht.chatRoom.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/chat/room")
 public class chatRoomController {
 
-    @GetMapping("chat/room/{roomId}")
+    @GetMapping("/{roomId}")
     @ResponseBody
     public String showRoom(
             @PathVariable("roomId") final long roomId,
             @RequestParam(value = "writeName", defaultValue = "NoName") final String writerName
     ) {
         return "%d번 채팅방 입니다. write : %s".formatted(roomId, writerName);
+    }
+
+    @GetMapping("/make")
+    @ResponseBody
+    public String showMak() {
+        return """
+                <form action="">
+                  <div>
+                    <input type="text">
+                  </div>
+                  <div>
+                    <input type="submit">
+                  </div>
+                </form>
+                """.stripIndent();
     }
 }
